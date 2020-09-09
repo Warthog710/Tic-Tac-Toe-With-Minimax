6b2c4d97a84e0effb022f07db048a14012bbe0d4
Warthog710 <computergeek710@gmail.com>
2020-09-09 16:16:06 -0700

    added minimax AI player

--- tictac.py
+++ tictac.py
@@ -1,3 +1,5 @@
+import copy
+
 class gameField:
     def __init__(self):
         #Create private 2D List
@@ -20,20 +22,23 @@ class gameField:
     def checkWinConditions(self):
         temp = self.__checkHorizontals()
         if temp != False:
-            print(temp, "is the winner!")
-            return True
+            return temp
 
         temp = self.__checkVerticals()
         if temp != False:
-            print(temp, "is the winner!")
-            return True
+            return temp
 
         temp = self.__checkDiagonals()
         if temp != False:
-            print(temp, "is the winner!")
-            return True
+            return temp
 
-        return False 
+        #Check for tie condition
+        for x in range(0,3):
+            for y in range(0,3):
+                if self.isValid(x,y):
+                    return False
+
+        return 'T' 
 
     def placeCordinates(self, player, xCord, yCord):
         if player == 'X':
@@ -65,20 +70,23 @@ class gameField:
         #Check left diagonal
         if self.__field[0][0] == self.__field[1][1] == self.__field[2][2]:
             if self.__field[0][0] != '_' and self.__field[1][1] != '_' and self.__field[2][2] != '_':
-                return self.__field[0][0]
+                return self.__field[1][1]
         #Check right diagonal
         elif self.__field[0][2] == self.__field[1][1] == self.__field[2][0]:
             if self.__field[0][2] != '_' and self.__field[1][1] != '_' and self.__field[2][0] != '_':
-                return self.__field[0][0]
+                return self.__field[1][1]
 
         #Diagonal win condition not met
         return False
 
     def isValid(self, xCord, yCord):
         if self.__field[xCord][yCord] == '_':
-            return False
-        return True
+            return True
+        return False 
 
+class cordinates:
+    x = 0
+    y = 0
 
 def playerTurn(myGameField, player):
     while True:
@@ -89,7 +97,7 @@ def playerTurn(myGameField, player):
         #Check for errors...
         if not(int(x) < 3 and int(x) >= 0 and int(y) < 3 and int(y) >= 0):
             print("Your input was out of bounds. Both values must be between 0 and 2.")
-        elif myGameField.isValid(int(x), int(y)):
+        elif not(myGameField.isValid(int(x), int(y))):
             print("Someone has already played there!")
         else:
             break
@@ -97,31 +105,99 @@ def playerTurn(myGameField, player):
     #Place on game field
     myGameField.placeCordinates(player, int(x), int(y))
 
-#Gamefield Object
-myGameField = gameField()
+def computerTurn(myGameField, player):
+    cords = cordinates()
+    maxEval = -100
 
-#Gameloop (player X always goes first)
-while True:
-    print("Current game state:")
-    myGameField.printField()
+    #Iterate through all possible moves...
+    for x in range(0,3):
+        for y in range(0,3):
+            if myGameField.isValid(x,y):
+                    temp = copy.deepcopy(myGameField)
+                    temp.placeCordinates('X', x, y)
+                    evaluation = minimax(temp, 3, False)
+                    if maxEval < evaluation:
+                        cords.x = x
+                        cords.y = y
+                        maxEval = evaluation
+    
+    return cords
 
-    #Player X
-    playerTurn(myGameField, 'X')
-    print("Current game state:")
-    myGameField.printField()
+def minimax(myGameField, depth, maximizingPlayer):
+    #If win condition has been met or depth is zero
+    #Return the score Win = 1, lose = -1, otherwise zero
+    if myGameField.checkWinConditions() or depth == 0:
+        if myGameField.checkWinConditions() == 'X':
+            return 1
+        elif myGameField.checkWinConditions() == 'O':
+            return -1
+        else:
+            return 0
 
-    #Check state
-    if myGameField.checkWinConditions(): 
-        myGameField.printField()    
-        break
+    #If I am trying to maximize the player...
+    if maximizingPlayer:
+        maxEval = -100
+        for x in range(0,3):
+            for y in range(0,3):
+                if myGameField.isValid(x, y):
+                    temp = copy.deepcopy(myGameField)
+                    temp.placeCordinates('X', x, y)
+                    evaluation = minimax(temp, depth -1, False)
+                    maxEval = max(maxEval, evaluation)
+        return maxEval
 
-    #Player O
-    playerTurn(myGameField, 'O')
+    #Else, I must be minimizing the player
+    else:
+        minEval = 100
+        for x in range(0,3):
+            for y in range(0,3):
+                if myGameField.isValid(x,y):
+                    temp = copy.deepcopy(myGameField)
+                    temp.placeCordinates('O', x, y)
+                    evaluation = minimax(temp, depth - 1, True)
+                    minEval = min(minEval, evaluation)
+        return minEval
 
-    #Check state
-    if myGameField.checkWinConditions():
+def whoWon(winValue):
+    if winValue == 'X':
+        print("X won the game!")
+
+    elif winValue == 'O':
+        print("O won the game!")
+    
+    else:
+        print("Tie!")
+
+def playGame(myGameField):
+    #Gameloop (player X always goes first)
+    while True:        
+        #Player X (computer)
+        cords = computerTurn(myGameField, 'X')
+        myGameField.placeCordinates('X', cords.x, cords.y)
+        print("Current game state:")
         myGameField.printField()
-        break  
+
+        #Check state
+        temp = myGameField.checkWinConditions()
+        if temp: 
+            whoWon(temp)
+            myGameField.printField()
+            break
+
+        #Player O (human)
+        playerTurn(myGameField, 'O')
+
+        #Check state
+        temp = myGameField.checkWinConditions()
+        if temp: 
+            whoWon(temp)
+            myGameField.printField()
+            break
+
+#EXECUTION STARTS HERE!!!      
+    
+myGameField = gameField()
+playGame(myGameField)
 
 #Need to do
-#   Computer opponent (MINMIXAI)
+#   Add GUI (Tkinter?)
